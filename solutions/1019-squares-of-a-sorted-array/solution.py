4adaf692304c999c78600d007364eb5dc25aadf9
Hemanth-2809 <hemanthmandala70@gmail.com>
2025-07-26 02:39:38 +0000

    Sync LeetCode submission - Squares of a Sorted Array - Runtime - 3 ms (97.27%), Memory - 14.4 MB (20.27%)

---
+++ solutions/1019-squares-of-a-sorted-array/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        nums = [num**2 for num in nums]
+        return sorted(nums)
+                
+        
