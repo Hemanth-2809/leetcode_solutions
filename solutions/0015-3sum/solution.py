4a2b0d65bb14e2f1a30369cc2c7c2f41c7fc4415
Hemanth-2809 <hemanthmandala70@gmail.com>
2025-07-27 01:51:26 +0000

    Sync LeetCode submission - 3Sum - Runtime - 716 ms (78.34%), Memory - 18.5 MB (41.83%)

---
+++ solutions/0015-3sum/solution.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        sol = []
+        nums = sorted(nums)
+        for i in range(len(nums)):
+            left = i+1
+            right = len(nums)-1
+            if i> 0 and nums[i] == nums[i-1]:
+                continue
+            while left < right:
+                sum =  nums[i]+nums[left]+nums[right]
+                
+                if sum < 0:
+                    left +=1
+                elif sum > 0:
+                    right -=1
+                else:
+                    sol.append([nums[i],nums[left],nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left+=1
+                    while left< right and  nums[right] == nums[right-1]:
+                        right-=1
+                    left += 1
+                    right -= 1
+                     
+        return sol
+
+
+
+        
