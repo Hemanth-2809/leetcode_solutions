4894a0f6801353f95de375902903d1a8aca6c153
Hemanth-2809 <hemanthmandala70@gmail.com>
2025-08-11 00:10:19 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock - Runtime - 43 ms (73.41%), Memory - 19.5 MB (13.49%)

---
+++ solutions/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        max_profit = 0
+        min_price = 9999999
+        for i in range(len(prices)):
+            if prices[i]<min_price:
+                min_price = prices[i]
+            if (prices[i] - min_price)> max_profit:
+                max_profit = prices[i] - min_price
+        return max_profit 
+        
+
+        
