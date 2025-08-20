77ab1bf69fdabc800091143901c2009ddef91e7e
Hemanth-2809 <hemanthmandala70@gmail.com>
2025-08-20 00:55:14 +0000

    Sync LeetCode submission - Sqrt(x) - Runtime - 7 ms (26.60%), Memory - 12.3 MB (54.57%)

---
+++ solutions/0069-sqrtx/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        l = 1
+        h = x
+        while l<=h:
+            mid = (l+h)//2
+            if mid**2 == x:
+                return mid
+            elif mid**2 > x:
+                h = mid-1
+            else:
+                l = mid+1
+        return h
+        
