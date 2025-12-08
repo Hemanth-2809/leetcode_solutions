5b82d99a3931348be57b9ac3dedb9bafaa130134
Hemanth-2809 <hemanthmandala70@gmail.com>
2025-12-08 01:44:53 +0000

    Sync LeetCode submission - Palindrome Linked List - Runtime - 132 ms (46.52%), Memory - 66.2 MB (80.34%)

---
+++ solutions/0234-palindrome-linked-list/solution.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: bool
+        """
+        slow = head 
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        
+        prev = None
+        curr = slow
+
+        while curr:
+            nxt = curr.next    
+            curr.next = prev   
+            prev = curr        
+            curr = nxt
+        
+        while prev:
+            if prev.val != head.val:
+                return False
+            prev = prev.next
+            head = head.next
+        return True
+        
