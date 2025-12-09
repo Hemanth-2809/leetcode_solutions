e920aaa6fb0c043f7a9787b9c3699e952505898b
Hemanth-2809 <hemanthmandala70@gmail.com>
2025-12-09 00:54:37 +0000

    Sync LeetCode submission - Intersection of Two Linked Lists - Runtime - 299 ms (5.04%), Memory - 66.7 MB (82.27%)

---
+++ solutions/0160-intersection-of-two-linked-lists/solution.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        if not headA or not headB:
+            return None
+        temp1 = headA
+        temp2 = headB
+        while temp1 != temp2:
+            if temp1 == None:
+                temp1 = headB
+            else:
+                temp1 = temp1.next
+            if temp2 == None:
+                temp2 = headA
+            else:
+                temp2 = temp2.next
+        return temp1
+        
+        
