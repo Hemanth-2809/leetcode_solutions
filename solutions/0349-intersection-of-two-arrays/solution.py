7e861bc22763c2d03ebb9405fcbb775a9c7b3daf
Hemanth-2809 <hemanthmandala70@gmail.com>
2025-12-12 00:54:11 +0000

    Sync LeetCode submission - Intersection of Two Arrays - Runtime - 0 ms (100.00%), Memory - 12.5 MB (30.60%)

--- solutions/0349-intersection-of-two-arrays/solution.py
+++ solutions/0349-intersection-of-two-arrays/solution.py
@@ -5,20 +5,23 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        nums1.sort()
-        nums2.sort()
-        nums_intr = []
-        i = 0
-        j = 0
-        while i < len(nums1)and j <len(nums2):
-            if nums1[i] == nums2[j] :
-                if nums1[i] not in nums_intr:
-                    nums_intr.append(nums1[i])
-                j+=1
-                i+=1
-            elif nums1[i] < nums2[j]:
-                i +=1
-            elif nums2[j] < nums1[i]:
-                j+=1
-        return nums_intr
+        numm1 = set(nums1)
+        numm2 = set(nums2)
+        sol = []
+        for x in numm1:
+            if x in numm2:
+                sol.append(x)
+        return sol
+        
+
+
+
+
+
+
+
+
+
+
+
         
