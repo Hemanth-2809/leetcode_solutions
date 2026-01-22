650937388cb888eeac782f1063c7f729ad484af1
Hemanth-2809 <hemanthmandala70@gmail.com>
2026-01-22 09:56:19 +0000

    Sync LeetCode submission - Valid Palindrome II - Runtime - 123 ms (5.06%), Memory - 12.7 MB (19.90%)

---
+++ solutions/0680-valid-palindrome-ii/solution.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        def fun(i,j):
+            while i<j:
+                if s[i]!=s[j]:
+                    return False
+                i+=1
+                j-=1
+            
+            return True
+        i = 0
+        j = len(s)-1
+        while i<j:
+            if s[i]!=s[j]:
+                return (fun((i+1),j) or fun(i,(j-1)))
+            i+=1
+            j-=1
+        return True
+
+            
+        
+        
+        
+
+
+        
