562492820d26dd0516a39916a5ef6eb4ea08667c
Hemanth-2809 <hemanthmandala70@gmail.com>
2026-02-25 15:57:44 +0000

    Sync LeetCode submission - Integer Break - Runtime - 10 ms (9.33%), Memory - 12.5 MB (1.55%)

--- solutions/0343-integer-break/solution.py
+++ solutions/0343-integer-break/solution.py
@@ -4,17 +4,18 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp ={}
-        def f(num):
-            if num == 1:
+        dp = {}
+        def f(n):
+            if n == 1:
                 return 1
-            if num in dp:
-                return dp[num]
-            dp[num] = 0 if num == n else num
-            for i in range(1, num):
-                val = f(i) * f(num - i)
-                dp[num] = max(dp[num], val)
-            return dp[num]
+            if n in dp:
+                return dp[n]
+            
+            val = 0
+            for i in range(1,n):
+                val = max(val,max(i,f(i))*max(n-i,f(n-i)))
+            dp[n] = val
+            return val
         return f(n)
-
+        
         
