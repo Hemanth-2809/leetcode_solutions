baaa81e4624db884915151a2b5f5928099f8824c
Hemanth-2809 <hemanthmandala70@gmail.com>
2026-03-02 16:56:54 +0000

    Sync LeetCode submission - 3Sum - Runtime - 657 ms (85.27%), Memory - 18.3 MB (80.61%)

--- solutions/0015-3sum/solution.py
+++ solutions/0015-3sum/solution.py
@@ -4,31 +4,34 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        sol = []
-        nums = sorted(nums)
+        nums.sort()
+        res = []
         for i in range(len(nums)):
-            left = i+1
-            right = len(nums)-1
-            if i> 0 and nums[i] == nums[i-1]:
+            if i>0 and nums[i] == nums[i-1]:
                 continue
-            while left < right:
-                sum =  nums[i]+nums[left]+nums[right]
-                
-                if sum < 0:
-                    left +=1
-                elif sum > 0:
-                    right -=1
+            x = nums[i]
+            j = i+1
+            k = len(nums)-1
+            while j<k:
+                y = nums[j]+nums[k]
+                if  y > -x:
+                    k-=1
+                elif y < -x:
+                    j+=1
                 else:
-                    sol.append([nums[i],nums[left],nums[right]])
-                    while left < right and nums[left] == nums[left+1]:
-                        left+=1
-                    while left< right and  nums[right] == nums[right-1]:
-                        right-=1
-                    left += 1
-                    right -= 1
-                     
-        return sol
+                    res.append([x,nums[j],nums[k]])
+                    j+=1
+                    k-=1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+        return res
 
 
+                    
+
+                
+        
 
         
