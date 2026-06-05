ea9152b37b4c1f33efb0f7a163c9f2bc1c06e115
Hemanth-2809 <hemanthmandala70@gmail.com>
2026-06-05 14:37:27 +0000

    Sync LeetCode submission - Find the City With the Smallest Number of Neighbors at a Threshold Distance - Runtime - 10 ms (67.76%), Memory - 46.6 MB (43.17%)

--- solutions/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/solution.java
+++ solutions/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/solution.java
@@ -1,42 +1,42 @@
 class Solution {
-    public int bellman(int n, int[][] edges, int dt) {
-
-        int[][] distarr = new int[n][n];
-
+    public int floyd(int n, int[][] edges, int distanceThreshold) {
+        int dist[][] = new int[n][n];
         for (int i = 0; i < n; i++) {
-            Arrays.fill(distarr[i], Integer.MAX_VALUE);
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+            dist[i][i] = 0;
+
         }
-        int[] thresh = new int[n];
-        for (int i = 0; i < n; i++) {
-            distarr[i][i] = 0;
-            for (int j = 0; j < n; j++) {
-                for (int[] ed : edges) {
-                    int u = ed[0];
-                    int v = ed[1];
-                    int dis = ed[2];
-                    if (distarr[i][u] != Integer.MAX_VALUE && distarr[i][u] + dis < distarr[i][v]) {
-                        distarr[i][v] = distarr[i][u] + dis;
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
 
+            dist[u][v] = w;
+            dist[v][u] = w;
+        }
+
+        int[] thresh = new int[n];
+        for (int via = 0; via < n; via++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][via] == Integer.MAX_VALUE || dist[via][j] == Integer.MAX_VALUE) {
+                        continue;
                     }
-                    if (distarr[i][v] != Integer.MAX_VALUE && distarr[i][v] + dis < distarr[i][u]) {
-                        distarr[i][u] = distarr[i][v] + dis;// as birectional
-                    }
+                    dist[i][j] = Math.min(dist[i][j], dist[i][via] + dist[via][j]);
 
                 }
-
             }
-
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i != j && distarr[i][j] <= dt) {
+                if (i != j && dist[i][j] <=distanceThreshold) {
                     thresh[i]++;
                 }
             }
         }
         int ans = 0;
-        for(int i= 0;i<n;i++){
-            if(thresh[i]<= thresh[ans]){
+        for (int i = 0; i < n; i++) {
+            if (thresh[i] <= thresh[ans]) {
                 ans = i;
             }
         }
@@ -45,7 +45,8 @@ class Solution {
     }
 
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        return floyd(n, edges, distanceThreshold);
 
-    return bellman(n,edges,distanceThreshold);
+    }
 }
-}
+
