29c71e76acb96878e10d76b7d05481a99fd92a10
Hemanth-2809 <hemanthmandala70@gmail.com>
2026-08-05 12:49:54 +0000

    Sync LeetCode submission - Assign Cookies - Runtime - 31 ms (75.85%), Memory - 14.2 MB (15.24%)

--- solutions/0455-assign-cookies/solution.py
+++ solutions/0455-assign-cookies/solution.py
@@ -5,15 +5,18 @@ class Solution(object):
         :type s: List[int]
         :rtype: int
         """
-        g.sort()
-        s.sort()
+        g=sorted(g,reverse = True)
+        s =sorted(s,reverse = True)
         i = 0
-        j = 0 
-        while i < len(g) and j<len(s):
-            if g[i] <= s[j]:
+        ans = 0
+        j = 0
+        while i <len(s) and j<len(g):
+            if s[i]>=g[j]:
+                ans+=1
                 i+=1
-            j+=1
-        return i
-            
+                j+=1
 
+            else:
+                j+=1
+        return ans
         
