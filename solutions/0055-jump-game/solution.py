e327d54d17815e5a8e54784e5a88765c2d2d0393
Hemanth-2809 <hemanthmandala70@gmail.com>
2026-08-09 20:22:41 +0000

    Sync LeetCode submission - Jump Game - Runtime - 37 ms (52.61%), Memory - 13.4 MB (29.55%)

--- solutions/0055-jump-game/solution.py
+++ solutions/0055-jump-game/solution.py
@@ -1,34 +1,23 @@
 class Solution(object):
-    '''
     def canJump(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        dp = {}
-        def f(i):
-            if i >= len(nums)-1:
+        dist = 0
+        n = len(nums)
+        for i in range(n):
+            nums[i]= nums[i]+i
+        nums[n-1] = n-1
+
+        i =0
+        while i <= dist:
+            dist = max(nums[i],dist)
+
+            i+=1
+            if dist >= n-1:
                 return True
-            elif i in dp:
-                return dp[i]
-            else:
-                
-                for j in range(i+1,min(len(nums), i + nums[i] + 1)):
-                    if f(j):
-                        dp[i] = True
-                        return True
-                dp[i] = False
-                return False
-        return f(0)'''
-        
-    def canJump(self, nums):
-        max_reach = 0
 
-        for i in range(len(nums)):
-            if i > max_reach:
-                return False
-            max_reach = max(max_reach, i + nums[i])
+        return False
 
-        return True
-        
-    
+
